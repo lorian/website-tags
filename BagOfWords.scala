205c1e7e938ea71213867fc8b9ef80a84279d154
Lorian <lanthala@gmail.com>
2016-07-11 19:17:58 -0700

    Assumes html tags have been stripped, and .txt file is in same directory as script

--- BagOfWords.scala
+++ BagOfWords.scala
@@ -1,6 +1,7 @@
-// quick and dirty word count that tries to strip html tags (but doesn't get them all)
+// quick and dirty word count after html tabs have been stripped
+// cat 000139__http___www_dell_com_us_business_p_networking_products.html | lynx --stdin --dump > 000139__http___www_dell_com_us_business_p_networking_products.txt 
 // run via spark-shell -i BagOfWords.scala
-val textFile = sc.textFile("cdips_dell/000139__http___www_dell_com_us_business_p_networking_products.html")
+val textFile = sc.textFile("000139__http___www_dell_com_us_business_p_networking_products.txt")
 val counts = textFile.map(line => line.replaceAll("""<(?!\/?a(?=>|\s.*>))\/?.*?>""", "")).flatMap(line => line.split(" ")).flatMap(line => line.split("\t")).map(word => (word, 1)).reduceByKey(_ + _)
 counts.saveAsTextFile("bagofwords")
 System.exit(0)
