0379c29a7142e8306c80dc7ac7e607c8d2f5d3f8
Lorian <lanthala@gmail.com>
2016-07-12 13:02:37 -0700

    Shell script that creates wordcount file for each .html file in current directory; takes 24 minutes to run on cdips_dell

---
+++ bagofwords.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for FILE in *.html
+do
+	# to avoid "File name too long" errors when we add new extentions:
+	FILEBASE=$(basename $FILE .html)
+	# strips html from file
+	cat $FILE | lynx --stdin --dump > ${FILEBASE}.txt
+	# gets the lines before References (the line number where References is comes from the grep command), 
+	# then converts all whitespace to spaces, removes duplicate spaces, removes everything containing a non-letter, inserts line returns between words, 
+	# lowercases everything, counts words, then saves output as text file
+	head -n $((`grep -nx 'References' ${FILEBASE}.txt | grep -Eo '^[^:]+'` -1)) ${FILEBASE}.txt | tr '[:space:]' ' ' |tr -c '[:alpha:] ' ' ' | tr -s ' '|  tr ' ' '\n' | tr '[:upper:]' '[:lower:]' | sort | uniq -c | > ${FILEBASE}.w.txt
+done
