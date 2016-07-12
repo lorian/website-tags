6e6c36224e058141978809d45d1cdcbd60b24dca
Lorian <lanthala@gmail.com>
2016-07-11 18:51:13 -0700

    Now tries to strip out html tags, but doesn't catch ones with spaces before the >

--- BagOfWords.scala
+++ BagOfWords.scala
@@ -1,6 +1,6 @@
-// quick and dirty word count that includes html tags
+// quick and dirty word count that tries to strip html tags (but doesn't get them all)
 // run via spark-shell -i BagOfWords.scala
 val textFile = sc.textFile("cdips_dell/000139__http___www_dell_com_us_business_p_networking_products.html")
-val counts = textFile.flatMap(line => line.split(" ")).flatMap(line => line.split("<")).flatMap(line => line.split(">")).flatMap(line => line.split("\t")).map(word => (word, 1)).reduceByKey(_ + _)
+val counts = textFile.map(line => line.replaceAll("""<(?!\/?a(?=>|\s.*>))\/?.*?>""", "")).flatMap(line => line.split(" ")).flatMap(line => line.split("\t")).map(word => (word, 1)).reduceByKey(_ + _)
 counts.saveAsTextFile("bagofwords")
 System.exit(0)
