f407cc4597110319668b7d0c0e46423174cc8212
bbgky <feihepku@gmail.com>
2016-07-12 22:08:26 -0700

    remove the last vertical bar, now it solves the problem

--- webETL.sh
+++ webETL.sh
@@ -2,5 +2,5 @@
 for FILE in *.html
 do
 	# strips html
-	cat $FILE | lynx --stdin --dump | sed -n '/References$/q;p'| tr '[:space:]' ' ' | tr -c '[:alpha:]' ' ' | tr -s ' ' | tr ' ' '\n' | tr '[:upper:]' '[:lower:]' | sort | uniq -c | > ${FILE:0:7}_count.txt
+	cat $FILE | lynx --stdin --dump | sed -n '/References$/q;p'| tr '[:space:]' ' ' | tr -c '[:alpha:]' ' ' | tr -s ' ' | tr ' ' '\n' | tr '[:upper:]' '[:lower:]' | sort | uniq -c > ${FILE:0:7}_count.txt
 done
