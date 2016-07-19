30e3678fd3a0b663106737fe10dc72d79a7c2046
Lorian <lanthala@gmail.com>
2016-07-19 12:01:26 -0700

    Converts wordcount dictionaries to sparse array, and clusters using kmeans

--- bagofwords.py
+++ bagofwords.py
@@ -3,22 +3,30 @@
 import pandas
 import os
 import pprint
+import matplotlib.pylab as plt
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.cluster import KMeans
 
+# Convert wordcount files to list of dictionaries for each page
 wordcounts_f = [f for f in os.listdir('.') if f.endswith('.w.txt')]
 wordcounts = []
 for fl in wordcounts_f:
 	wc_dict = dict()
-	wc_dict['File'] = fl
 	for line in open(fl, 'r'):
 		count = line.strip().partition(' ')
-		wc_dict[count[2]] = int(count[0])
+		if count[2]: # drop empty lines
+			wc_dict[count[2]] = int(count[0])
 	wordcounts.append(wc_dict)
-		
-#pprint.pprint(wordcounts)
 
-vec = DictVectorizer()
-vec.fit_transform(wordcounts).toarray()
-print vec.get_feature_names()
+# Use DictVectorizer to convert the dictionaries to a sparse array like sklearn wants
+word_features = DictVectorizer().fit_transform(wordcounts).toarray()
+#print word_features.get_feature_names()
 
+# k-means clustering
+clusters = KMeans(n_clusters=26).fit_predict(word_features)
+print clusters
+
+# Plot clusters
+plt.scatter(range(1,len(clusters)+1), clusters)
+plt.show()
