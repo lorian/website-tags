1a89456ed9e30874c926eeaf716296f679203492
Lorian <lanthala@gmail.com>
2016-07-27 21:07:49 -0700

    Now with attractive colored plots!

--- word_correlation_matrix.py
+++ word_correlation_matrix.py
@@ -2,6 +2,7 @@ import csv
 import argparse
 import pandas
 import numpy
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='Imports wordcounts from csv and determines word correlations')
 parser.add_argument('wordcount_file', help='csv containing table of wordcounts per page')
@@ -21,6 +22,20 @@ wc.drop(wc.index[:1], inplace=True)
 wc_nums = wc.drop('URL',1).astype(float)
 wc_corr = wc_nums.corr()
 
+# plot correlations of top 50 words
+wc_top = wc_nums.ix[:,0:50]
+correlations = wc_top.corr()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+fig.colorbar(ax.matshow(correlations, vmin=-1, vmax=1))
+ax.set_xticks(numpy.arange(correlations.shape[1]))
+ax.set_yticks(numpy.arange(correlations.shape[0]))
+ax.set_xticklabels(wc_top.columns, rotation=90)
+ax.set_yticklabels(wc_top.columns)
+plt.show()
+
 # order by correlation
 corr = wc_corr.unstack().order(kind="quicksort").to_frame()
-print corr[corr.index.get_level_values(0) != corr.index.get_level_values(1)][corr[0]>.9]
+output = corr[corr.index.get_level_values(0) != corr.index.get_level_values(1)][corr[0]>.9]
+
